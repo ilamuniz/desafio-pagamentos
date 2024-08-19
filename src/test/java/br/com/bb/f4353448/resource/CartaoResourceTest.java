5ff8cfaf043c993257b289b589e274e78d232ae3
F4353448 Ila Mascarenhas Muniz <ila.muniz@bb.com.br>
2024-08-19 15:07:32 -0300

    Inclusão de testes unitários na classe CartaoResourceTest

--- src/test/java/br/com/bb/f4353448/resource/CartaoResourceTest.java
+++ src/test/java/br/com/bb/f4353448/resource/CartaoResourceTest.java
@@ -10,9 +10,56 @@ public class CartaoResourceTest {
     @Test
     public void statusCodeDaRequisicao200() {
         RestAssured.given()
-                .get("pagamentos")
+                .get("/pagamentos")
                 .then()
                 .statusCode(200);
     }
 
+    @Test
+    public void dadosNulosStatusCode400() {
+        String dadosNulos = "{ \"tipoPessoa\": 0, \"cpfOuCnpj\": \"\", \"pagamento\": 0.00, \"nomeTitular\": \"\", \"numeroDoCartao\": \"\", \"mesVencimento\": 0, \"anoVencimento\": 0, \"codigoDeSeguranca\": \"\" }";
+        RestAssured.given()
+                .contentType("application/json")
+                .body(dadosNulos)
+                .when()
+                .post("/pagamentos")
+                .then()
+                .statusCode(400);
+    }
+
+    @Test
+    public  void preenchimentoIncorretoCPFStatusCode422() {
+        String cpfIncorreto = "{ \"tipoPessoa\": 1, \"cpfOuCnpj\": \"3333\", \"pagamento\": 300.00, \"nomeTitular\": \"Luiz\", \"numeroDoCartao\": \"1234567891011\", \"mesVencimento\": 4, \"anoVencimento\": 2036, \"codigoDeSeguranca\": \"123\" }";
+        RestAssured.given()
+                .contentType("application/json")
+                .body(cpfIncorreto)
+                .when()
+                .post("/pagamentos")
+                .then()
+                .statusCode(422);
+    }
+
+    @Test
+    public void preenchimentoIncorretoNumeroCartaoStatusCode422() {
+        String numeroCartaoIncorreto = "{ \"tipoPessoa\": 1, \"cpfOuCnpj\": \"33333333333\", \"pagamento\": 300.00, \"nomeTitular\": \"Luiz\", \"numeroDoCartao\": \"1234\", \"mesVencimento\": 4, \"anoVencimento\": 2036, \"codigoDeSeguranca\": \"123\" }";
+        RestAssured.given()
+                .contentType("application/json")
+                .body(numeroCartaoIncorreto)
+                .when()
+                .post("/pagamentos")
+                .then()
+                .statusCode(422);
+    }
+
+    @Test
+    public void preencherSomenteNumerosStatusCode422() {
+        String somenteNumeros = "{ \"tipoPessoa\": 1, \"cpfOuCnpj\": \"33333333333\", \"pagamento\": 300.00, \"nomeTitular\": \"Luiz\", \"numeroDoCartao\": \"123456789abcd\", \"mesVencimento\": 4, \"anoVencimento\": 2036, \"codigoDeSeguranca\": \"123\" }";
+        RestAssured.given()
+                .contentType("application/json")
+                .body(somenteNumeros)
+                .when()
+                .post("/pagamentos")
+                .then()
+                .statusCode(422);
+    }
 }
