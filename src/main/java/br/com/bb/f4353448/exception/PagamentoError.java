c280f9f9b079db97c3bcfce13a903322892dc3ff
F4353448 Ila Mascarenhas Muniz <ila.muniz@bb.com.br>
2024-08-29 15:09:36 -0300

    Criação de exception personalizada

---
+++ src/main/java/br/com/bb/f4353448/exception/PagamentoError.java
@@ -0,0 +1,18 @@
+package br.com.bb.f4353448.exception;
+
+public interface PagamentoError {
+    String MSG_NULO = "A mensagem não pode ser nula.";
+    String CODIGO_NULO = "O código não pode ser nulo.";
+
+    static String getCode();
+
+    String getMessage();
+
+    default String getMessage(Object... messageArgs) {
+        return String.format(this.getMessage(), messageArgs);
+    }
+
+    default int getStatusCode() {
+        return 422;
+    }
+}
